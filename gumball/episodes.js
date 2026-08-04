23c66491035cfd099c56fe5d67e492172edb1002
2oonzee <robloxtoobers932021@gmail.com>
2026-08-04 15:29:00 +0300

    Create episodes.js

---
+++ gumball/episodes.js
@@ -0,0 +1,80 @@
+// ==========================================
+// Stardima Episodes Database
+// Gumball
+// ==========================================
+
+const seasons = {
+
+    1: [
+        {
+            title: "الحلقة 1",
+            players: {
+                one: "",
+                two: "",
+                three: ""
+            },
+            download: ""
+        }
+    ],
+
+    2: [
+        {
+            title: "الحلقة 1",
+            players: {
+                one: "",
+                two: "",
+                three: ""
+            },
+            download: ""
+        }
+    ],
+
+    3: [
+        {
+            title: "الحلقة 1",
+            players: {
+                one: "",
+                two: "",
+                three: ""
+            },
+            download: ""
+        }
+    ],
+
+    4: [
+        {
+            title: "الحلقة 1",
+            players: {
+                one: "",
+                two: "",
+                three: ""
+            },
+            download: ""
+        }
+    ],
+
+    5: [
+        {
+            title: "الحلقة 1",
+            players: {
+                one: "",
+                two: "",
+                three: ""
+            },
+            download: ""
+        }
+    ],
+
+    6: [
+        {
+            title: "الحلقة 1",
+            players: {
+                one: "",
+                two: "",
+                three: ""
+            },
+            download: ""
+        }
+    ]
+
+};
